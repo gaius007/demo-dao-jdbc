aee3d0cf91931c278b013435bc22348dc575ba9b
Caio de Paula Oliveira <c.oliveira2005@outlook.com>
2025-01-08 12:44:11 -0300

    Reusing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -53,17 +53,9 @@ public class SellerDaoJDBC implements SellerDao {
 			rs = st.executeQuery();
 			
 			if (rs.next()) {
-				Department department = new Department(); 
-				department.setId(rs.getInt("DepartmentId"));
-				department.setName(rs.getString("DepartmentName"));
+				Department department = intantiateDepartment(rs);
 				
-				Seller seller = new Seller();
-				seller.setId(rs.getInt("Id"));
-				seller.setName(rs.getString("Name"));
-				seller.setEmail(rs.getString("Email"));
-				seller.setBaseSalary(rs.getDouble("BaseSalary"));
-				seller.setBirthDate(rs.getDate("BirthDate"));
-				seller.setDepartment(department);
+				Seller seller = intantiateSeller(rs, department);
 				
 				return seller;
 			}
@@ -76,6 +68,26 @@ public class SellerDaoJDBC implements SellerDao {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
+	}
+
+	private Seller intantiateSeller(ResultSet rs, Department department) throws SQLException {
+		Seller seller = new Seller();
+		seller.setId(rs.getInt("Id"));
+		seller.setName(rs.getString("Name"));
+		seller.setEmail(rs.getString("Email"));
+		seller.setBaseSalary(rs.getDouble("BaseSalary"));
+		seller.setBirthDate(rs.getDate("BirthDate"));
+		seller.setDepartment(department);
+		
+		return seller;
+	}
+
+	private Department intantiateDepartment(ResultSet rs) throws SQLException {
+		Department department = new Department(); 
+		department.setId(rs.getInt("DepartmentId"));
+		department.setName(rs.getString("DepartmentName"));
+		
+		return department;
 	}
 
 	@Override
